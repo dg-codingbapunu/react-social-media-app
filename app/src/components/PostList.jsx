3758cf43304e7c9bdbac7475ee9ec202dc0bbf2b
patitapaban <patitapabanpanda046@gmail.com>
2024-04-29 14:01:53 +0530

    fetch data from dummy api

--- app/src/components/PostList.jsx
+++ app/src/components/PostList.jsx
@@ -1,11 +1,23 @@
 import { useContext } from "react";
 import Post from "./Post";
 import { PostList as PostListData } from "../components/store/post-list-store";
+import WelcomeMeassage from "./WelcomeMessage";
 
 const PostList = () => {
-  const { postList } = useContext(PostListData);
+  const { postList, addInitialPosts } = useContext(PostListData);
+  const handleGetPostsClick = () => {
+    fetch("https://dummyjson.com/products")
+      .then((res) => res.json())
+      .then((data) => {
+        addInitialPosts(data.posts);
+      });
+  };
+
   return (
     <>
+      {postList.length === 0 && (
+        <WelcomeMeassage onGetPostsClick={handleGetPostsClick} />
+      )}
       {postList.map((post) => (
         <Post key={post.id} post={post} />
       ))}
