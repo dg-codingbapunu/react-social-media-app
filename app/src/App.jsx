d945f311a33dbe3e1877310bc16c93838fc70c2c
patitapaban <patitapabanpanda046@gmail.com>
2024-04-29 10:46:57 +0530

    add dist

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,12 +1,12 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
-import Header from "../components/Header";
-import Footer from "../components/Footer";
-import Sidebar from "../components/Sidebar";
-import CreatePost from "../components/CreatePost";
-import PostList from "../components/PostList";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Sidebar from "./components/Sidebar";
+import CreatePost from "./components/CreatePost";
+import PostList from "./components/PostList";
 import { useState } from "react";
-import PostListProvider from "../components/store/post-list-store";
+import PostListProvider from "./components/store/post-list-store";
 
 function App() {
   const [selectedTab, setSelectedTab] = useState("Home");
