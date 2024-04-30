af2274abae4e607899a30263638dfdb7de57d4d7
patitapaban <patitapabanpanda046@gmail.com>
2024-04-30 10:55:11 +0530

    use effect hook

--- app/src/components/PostList.jsx
+++ app/src/components/PostList.jsx
@@ -1,26 +1,24 @@
-import { useContext, useState } from "react";
+import { useContext, useEffect } from "react";
 import Post from "./Post";
 import { PostList as PostListData } from "../components/store/post-list-store";
-import WelcomeMessage from "./WelcomeMessage"; // Corrected import
+import WelcomeMessage from "./WelcomeMessage";
 
 const PostList = () => {
   const { postList, addInitialPosts } = useContext(PostListData);
-  const [dataFatched, setDataFetched] = useState(false);
-  if (!dataFatched) {
+  useEffect(() => {
     fetch("https://dummyjson.com/products")
       .then((res) => res.json())
       .then((data) => {
         addInitialPosts(data.posts);
       });
-    setDataFetched(true);
-  }
+    [];
+  });
+
   const handleGetPostsClick = () => {};
 
   return (
     <>
-      {postList.length === 0 && (
-        <WelcomeMessage onGetPostsClick={handleGetPostsClick} />
-      )}
+      {postList.length === 0 && <WelcomeMessage />}
       {postList.map((post) => (
         <Post key={post.id} post={post} />
       ))}
