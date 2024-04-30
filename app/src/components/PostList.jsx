093bfbeb4476711edb6d7634520a3cc11317b979
patitapaban <patitapabanpanda046@gmail.com>
2024-04-30 10:32:17 +0530

    update

--- app/src/components/PostList.jsx
+++ app/src/components/PostList.jsx
@@ -1,22 +1,25 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import Post from "./Post";
 import { PostList as PostListData } from "../components/store/post-list-store";
 import WelcomeMessage from "./WelcomeMessage"; // Corrected import
 
 const PostList = () => {
   const { postList, addInitialPosts } = useContext(PostListData);
-  const handleGetPostsClick = () => {
+  const [dataFatched, setDataFetched] = useState(false);
+  if (!dataFatched) {
     fetch("https://dummyjson.com/products")
       .then((res) => res.json())
       .then((data) => {
         addInitialPosts(data.posts);
       });
-  };
+    setDataFetched(true);
+  }
+  const handleGetPostsClick = () => {};
 
   return (
     <>
       {postList.length === 0 && (
-        <WelcomeMessage onGetPostsClick={handleGetPostsClick} /> // Corrected component name
+        <WelcomeMessage onGetPostsClick={handleGetPostsClick} />
       )}
       {postList.map((post) => (
         <Post key={post.id} post={post} />
--- app/src/components/WelcomeMessage.jsx
+++ app/src/components/WelcomeMessage.jsx
@@ -2,13 +2,6 @@ const WelcomeMeassage = ({ onGetPostsClick }) => {
   return (
     <center className="welcome-message">
       <h1>There are no posts</h1>
-      <button
-        type="button"
-        onClick={onGetPostsClick}
-        className="btn btn-primary"
-      >
-        Get post From Server
-      </button>
     </center>
   );
 };
