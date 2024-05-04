aea91c6bfe930f7f22b52053cb297c12ae614950
patitapaban <patitapabanpanda046@gmail.com>
2024-05-04 09:55:39 +0530

    update

--- app/src/components/Sidebar.jsx
+++ app/src/components/Sidebar.jsx
@@ -4,15 +4,15 @@ const Sidebar = () => {
       className="d-flex flex-column flex-shrink-0 p-3 text-bg-dark sidebar"
       style={{ width: "180px" }}
     >
-      <a
-        href="/"
+      <link
+        to="/"
         className="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none"
       >
         <svg className="bi pe-none me-2" width="40" height="32">
           <use xlinkHref="#bootstrap"></use>
         </svg>
         <span className="fs-4">Sidebar</span>
-      </a>
+      </link>
       <hr />
       <ul className="nav nav-pills flex-column mb-auto">
         <li className="nav-item">
@@ -24,12 +24,12 @@ const Sidebar = () => {
           </a>
         </li>
         <li>
-          <a href="/create-post" className="nav-link text-white">
+          <link href="/create-post" className="nav-link text-white">
             <svg className="bi pe-none me-2" width="16" height="16">
               <use xlinkHref="#speedometer2"></use>
             </svg>
             Create Post
-          </a>
+          </link>
         </li>
       </ul>
       <hr />
