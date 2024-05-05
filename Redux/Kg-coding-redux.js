1e2efdf72f0aa71e4d4e256f6c5a8370786ceb25
patitapaban <patitapabanpanda046@gmail.com>
2024-05-05 10:40:56 +0530

    subscriber in rerdux

--- Redux/Kg-coding-redux.js
+++ Redux/Kg-coding-redux.js
@@ -2,3 +2,9 @@ const redux = require("redux");
 const reducer = (store, action) => {};
 
 const store = redux.createStore(reducer);
+const subscriber = () => {
+  const state = store.getState();
+  console.log(state);
+};
+
+store.subscribe(subscriber);
