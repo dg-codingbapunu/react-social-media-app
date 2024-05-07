0c13efd781cfa84b8f0af1922d3b7e48152c49f0
patitapaban <patitapabanpanda046@gmail.com>
2024-05-07 13:28:26 +0530

    compete counter using react-redux

--- counter/src/components/store/index.js
+++ counter/src/components/store/index.js
@@ -1,9 +1,14 @@
 import { createStore } from "redux";
 const INITIAL_VALUE = {
-  counter: 5,
+  counter: 0,
 };
 
 const counterReducer = (store = INITIAL_VALUE, action) => {
+  if (action.type === "INCREMENT") {
+    return { counter: store.counter + 1 };
+  } else if (action.type === "DECREMENT") {
+    return { counter: store.counter - 1 };
+  }
   return store;
 };
 
