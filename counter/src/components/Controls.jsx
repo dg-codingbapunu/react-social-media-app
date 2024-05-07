419c4c6207e878a20540b20703f0fe04d6d8e633
patitapaban <patitapabanpanda046@gmail.com>
2024-05-07 13:42:56 +0530

    update

--- counter/src/components/Controls.jsx
+++ counter/src/components/Controls.jsx
@@ -11,23 +11,25 @@ const Controls = () => {
   };
 
   return (
-    <div className="d-grid gap-2 d-sm-flex justify-content-sm-center">
-      <button
-        type="button"
-        className="btn btn-primary"
-        onClick={handleIncrement}
-      >
-        +1
-      </button>
+    <>
+      <div className="d-grid gap-2 d-sm-flex justify-content-sm-center">
+        <button
+          type="button"
+          className="btn btn-primary"
+          onClick={handleIncrement}
+        >
+          +1
+        </button>
 
-      <button
-        type="button"
-        className="btn btn-success"
-        onClick={handleDecrement}
-      >
-        -1
-      </button>
-    </div>
+        <button
+          type="button"
+          className="btn btn-success"
+          onClick={handleDecrement}
+        >
+          -1
+        </button>
+      </div>
+    </>
   );
 };
 
